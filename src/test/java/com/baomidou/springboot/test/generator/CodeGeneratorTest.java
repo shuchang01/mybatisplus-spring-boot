01131fe212569fa57f0b215178d0f553fe5f986b
Chris <chris.shang@qq.com>
2018-03-30 23:51:23 +0800

    update code gen

--- src/test/java/com/baomidou/springboot/test/generator/CodeGeneratorTest.java
+++ src/test/java/com/baomidou/springboot/test/generator/CodeGeneratorTest.java
@@ -22,7 +22,7 @@ public class CodeGeneratorTest {
         String packageName = "com.baomidou.springboot";
         boolean serviceNameStartWithI = true;  // false: user -> UserService, 设置成true: user -> IUserService
 //        generateByTables(serviceNameStartWithI, packageName, "user", "role");
-        generateByTables(serviceNameStartWithI, packageName, "t_feedback");
+        generateByTables(serviceNameStartWithI, packageName, "t_category");
     }
 
     private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
@@ -37,22 +37,23 @@ public class CodeGeneratorTest {
                 .setDriverName("com.mysql.jdbc.Driver");
         StrategyConfig strategyConfig = new StrategyConfig();
         strategyConfig
-                .setCapitalMode(true)
-//                .setEntityLombokModel(false)
-                .setEntityLombokModel(true)
-                .setDbColumnUnderline(true)
+                .setCapitalMode(true)           // 是否大写命名
+//                .setEntityLombokModel(false)  //【实体】是否为lombok模型（默认 false）
+                .setDbColumnUnderline(true)     // 表名、字段名、是否使用下划线命名（默认 false）
+                .setEntityBooleanColumnRemoveIsPrefix(true) // Boolean类型字段是否移除is前缀（默认 false）
+                .entityTableFieldAnnotationEnable(true)     // 是否生成实体时，生成字段注解
                 .setRestControllerStyle(true) // Controller -> RestController
-                .setTablePrefix("t_")
+                .setTablePrefix("t_")         // 表前缀
                 .setNaming(NamingStrategy.underline_to_camel)
-                .setInclude(tableNames); //修改替换成你需要的表名，多个表名传数组
-        config.setActiveRecord(false)
-                .setBaseResultMap(true) // XML BaseResultMap, default: false
-                .setBaseColumnList(true) // XML BaseColumn, default: false
-                .setEnableCache(false) // 是否在XxMapper.xml中添加二级缓存配置, default: true
-                .setAuthor("Chris")
-//                .setOutputDir("d:\\codeGen")
-                .setOutputDir("codeGen") // 当前项目工程根目录下
-                .setFileOverride(true);
+                .setInclude(tableNames);    //修改替换成你需要的表名，多个表名传数组
+        config.setActiveRecord(false)       // 开启 ActiveRecord 模式
+                .setBaseResultMap(true)     // XML BaseResultMap, default: false
+                .setBaseColumnList(true)    // XML BaseColumn, default: false
+                .setEnableCache(false)      // 是否在XxMapper.xml中添加二级缓存配置, default: true
+                .setAuthor("Chris")         // Coding Author
+                .setOutputDir("codeGen")    // 当前项目工程根目录下
+                .setFileOverride(true)      // 是否覆盖已有文件
+                ;
         if (!serviceNameStartWithI) {
             config.setServiceName("%sService");
         }
